26752135afa1133f02b6222d37685fc0d3cc105e
kkoshiya <kylekoshiya@gmail.com>
2021-07-14 15:57:37 -0400

    added css and blog

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import (Flask, render_template)
 app = Flask(__name__)
 
 @app.route('/')
-def photon():
+def index():
   return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+  return render_template('blog.html')
